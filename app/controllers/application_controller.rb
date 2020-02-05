366803baa48cbf2d94e516cf47f1845a802d05ee
Scott Hoisington <hoyzington@gmail.com>
2020-02-05 19:32:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class App < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   get "/" do
+    erb :"/app/views/new"
+  end
+  
+  get "/" do
+    
+  end
+  
+  get "/" do
+    
+  end
+  
+  get "/" do
     
   end
 
